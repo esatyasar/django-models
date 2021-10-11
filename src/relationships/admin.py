8581ef8acad638a05f8dd6fba640d8ccaef4630b
esatyasar <esatyasar91@gmail.com>
2021-10-11 22:35:51 +0300

    added onetoonefield

--- src/relationships/admin.py
+++ src/relationships/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Creator
+from .models import Creator,Language,Frameworks
 
 admin.site.register(Creator)
+admin.site.register(Language)
+admin.site.register(Frameworks)
+
 # Register your models here.
--- src/relationships/models.py
+++ src/relationships/models.py
@@ -1,9 +1,26 @@
 from django.db import models
 
 # Create your models here.
-class Creator (models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+
+class Creator(models.Model):
+    
+    
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
     
     def __str__(self):
-        return ({self.first_name})
+        return self.first_name
+        
+class Language(models.Model):
+    creator = models.OneToOneField(Creator, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+        
+class Frameworks(models.Model):
+    name = models.CharField(max_length=40)
+    dil = models.ForeignKey(Language, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name 
