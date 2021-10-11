050932c1ef877094e00d18068622ec86156b89d1
esatyasar <esatyasar91@gmail.com>
2021-10-11 22:20:20 +0300

    added new table

--- src/app/settings.py
+++ src/app/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'store',
+    'django_extensions',
+    'relationships',
 ]
 
 MIDDLEWARE = [
--- src/relationships/models.py
+++ src/relationships/models.py
@@ -1,13 +1,9 @@
 from django.db import models
-from django.db.models.fields import CharField
 
 # Create your models here.
-
-class Student (models.Model):
+class Creator (models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    number = models.IntegerField()
     
     def __str__(self):
-        return (f"{self.number} - {self.first_name}")
-
+        return ({self.first_name})
