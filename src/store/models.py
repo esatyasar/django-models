2ed7ffff1f2ae7f678821c52fa8c874bdaea7541
esatyasar <esatyasar91@gmail.com>
2021-10-11 21:16:56 +0300

    create super user

--- src/store/models.py
+++ src/store/models.py
@@ -3,7 +3,10 @@ from django.db.models.fields import CharField
 
 # Create your models here.
 
-class Product (models.Model):
-    name = models.CharField(max_length=50)
-    content = models.TextField(max_length=1000)
-    price = models.FloatField()
+class Student (models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    number = models.IntegerField()
+    
+    def __str__(self):
+        return (f"{self.number} - {self.first_name}")
